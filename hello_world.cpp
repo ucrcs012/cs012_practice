4873fff51455df94be9498e5b4a823ed38e650bb
ecarm002 <ecarm002@ucr.edu>
2013-05-06 19:14:59 -0400

    added hello world program code.

---
+++ hello_world.cpp
@@ -0,0 +1,19 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// @file hello_world.cpp 
+/// @brief Prints Hello World! to the screen.
+/// 
+/// @author (Student Name) [student_id@ucr.edu]
+/// @date May 2013
+///
+/// @par Plagiarism Section
+/// I hereby certify that the code in this file
+/// is ENTIRELY my own original work.
+//  ================== END ASSESSMENT HEADER ===============
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
