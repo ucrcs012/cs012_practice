66723581d6fcea41c52f0085aeea8d2f7b60b182
ecarm002 <ecarm002@ucr.edu>
2013-05-06 20:07:29 -0400

    updated header from current cs010 template.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,16 @@
 //  =============== BEGIN ASSESSMENT HEADER ================
-/// @file hello_world.cpp 
-/// @brief Prints Hello World! to the screen.
-/// 
-/// @author (Student Name) [student_id@ucr.edu]
-/// @date May 2013
+/// @file assignment<#>/main.cpp 
+/// @brief Assignment/Lab <#, e.g. 1,2,3..10> for CS 10 <quarter & year>
+///
+/// @author <Your name> [<Your UCRSub login email>]
+/// @date <Current date>
+///
+/// @par Enrollment Notes 
+///     Lecture Section: <e.g. 001>
+/// @par
+///     Lab Section: <e.g. 021>
+/// @par
+///     TA: <TA name>
 ///
 /// @par Plagiarism Section
 /// I hereby certify that the code in this file
