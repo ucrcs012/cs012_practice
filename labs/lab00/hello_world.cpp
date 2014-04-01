d49111f42644d6e102bf81a10a598f31f53cd5dc
Adam <akoeh001@cs.ucr.edu>
2014-04-01 09:30:56 -0400

    new header

--- labs/lab00/hello_world.cpp
+++ labs/lab00/hello_world.cpp
@@ -1,20 +1,9 @@
 //  =============== BEGIN ASSESSMENT HEADER ================
-/// @file assignment<#>/main.cpp 
-/// @brief Assignment/Lab <#, e.g. 1,2,3..10> for CS 10 <quarter & year>
+/// @file lab0.cpp
+/// @brief Lab 0
 ///
-/// @author <Your name> [<Your UCRSub login email>]
-/// @date <Current date>
-///
-/// @par Enrollment Notes 
-///     Lecture Section: <e.g. 001>
-/// @par
-///     Lab Section: <e.g. 021>
-/// @par
-///     TA: <TA name>
-///
-/// @par Plagiarism Section
-/// I hereby certify that the code in this file
-/// is ENTIRELY my own original work.
+/// @author Test Student [RSubEmail@ucr.edu]
+/// @date April 1, 2014
 //  ================== END ASSESSMENT HEADER ===============
 #include <iostream>
 
